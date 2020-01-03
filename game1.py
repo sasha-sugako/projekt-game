4fe23c4da567019e18276658ca9297e445ddfb32
sasha <aleksandrasugako@yandex.ru>
2020-01-03 12:39:21 +0300

    Передвижение и отрисовка уровней

--- game1.py
+++ game1.py
@@ -2,7 +2,7 @@ import os
 import pygame
 import sys
 FPS = 50
-size = WIDTH, HEIGHT = 500, 500
+size = WIDTH, HEIGHT = 1000, 500
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.init()
@@ -72,10 +72,86 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
+tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png'), 'stairs': load_image('box.png')}
+player_image = load_image('mar.png')
+
+tile_width = tile_height = 50
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = pos_x, pos_y
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.rect = self.image.get_rect().move(
+            tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
+
+    def moves(self, stor):
+        x, y = self.pos
+        if stor == 'up':
+            if y > 0 and level[y][x] == '#':
+                self.move(x, y - 1)
+        if stor == 'down':
+            if y < level_y and level[y][x] == '#':
+                self.move(x, y + 1)
+        if stor == 'left':
+            if x > 0 and level[y][x - 1] != '*':
+                self.move(x - 1, y)
+        if stor == 'right':
+            if x < level_x and level[y][x + 1] != '*':
+                self.move(x + 1, y)
+
+
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('stairs', x, y)
+            elif level[y][x] == '*':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+level = load_level('1.txt')
+player, level_x, level_y = generate_level(level)
+start_screen()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        key = pygame.key.get_pressed()
+        if key[pygame.K_DOWN]:
+            player.moves('down')
+        if key[pygame.K_UP]:
+            player.moves('up')
+        if key[pygame.K_LEFT]:
+            player.moves('left')
+        if key[pygame.K_RIGHT]:
+            player.moves('right')
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
 pygame.quit()
